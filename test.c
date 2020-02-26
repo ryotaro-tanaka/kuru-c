e30066410e1850ff2e28327a2f8b305b14c61102
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-26 22:59:46 +0900

    13.2.4

--- test.c
+++ test.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+// int arrayPrint(int x[]);
 
 int main(void)
 {
+    int array[] = {11, 22, 33, 44, 55};
+    int array2[] = {0, 0, 0, 0, 0};
+
+    memcpy(array, array2, sizeof array2);
+
+    for (size_t i = 0; i < sizeof(array) /sizeof(array[0]); i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    
     return 0;
 }
+
+// int arrayPrint(int array[]) {
+//     printf("%ld\n", sizeof array);
+
+//     // for (size_t i = 0; i < sizeof(array) /sizeof(array[0]); i++)
+//     // {
+//     //     printf("%d\n", array[i]);
+//     // }
+
+//     return 0;
+// }
