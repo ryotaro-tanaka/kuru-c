793e9fc0f6c31974420e639110522d57c80b2758
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-26 18:28:57 +0900

    11.3

--- test.c
+++ test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
+int sum(int, int);
+
 int main(void)
 {
-    double r;
-    do {
-        printf("your input:");
-        scanf("%lf", &r);
-    } while (r < 0);
-    
-    printf("%f\n", r * r);
-
+    printf("%d\n", sum(10, 20));
     return 0;
+}
+
+int sum(int i1, int i2)
+{
+    return i1 + i2;
 }
