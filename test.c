4903d82bbd0f954b44db85308651079bbaf6d29b
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-27 09:32:24 +0900

    14.1

--- test.c
+++ test.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-
-// int arrayPrint(int x[]);
+#include <ctype.h>
 
 int main(void)
 {
-    int array[] = {11, 22, 33, 44, 55};
-    int array2[] = {0, 0, 0, 0, 0};
+    char c = '2';
+    int i = 0;
 
-    memcpy(array, array2, sizeof array2);
-
-    for (size_t i = 0; i < sizeof(array) /sizeof(array[0]); i++)
+    if (isdigit(c))
     {
-        printf("%d\n", array[i]);
+        i = c - '0';
     }
-    
+
+    printf("%d\n", i);
     return 0;
 }
-
-// int arrayPrint(int array[]) {
-//     printf("%ld\n", sizeof array);
-
-//     // for (size_t i = 0; i < sizeof(array) /sizeof(array[0]); i++)
-//     // {
-//     //     printf("%d\n", array[i]);
-//     // }
-
-//     return 0;
-// }
