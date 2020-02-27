010946cc5d541bd651dbbe0ba353099d426f3ce8
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-02-27 10:20:24 +0900

    14.2.1

--- test.c
+++ test.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main(void)
 {
-    char c = '2';
-    int i = 0;
-
-    if (isdigit(c))
-    {
-        i = c - '0';
-    }
-
-    printf("%d\n", i);
+    char str[6] = {'M', 'A', 'R', 'I', 'O', '\0'};
+    printf("%s\n", str);
     return 0;
 }
