95897bb3098084eeca9e09451466a605f29743e0
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-07 12:14:40 +0900

    15.3.3

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-    char str1[256], str2[] = "ABC";
-    scanf("%s", str1);
-    printf("%d\n", strcmp(str1, str2));
+    char str[256] = "DRAGON";
+    scanf("%s", &str[6]);
+    printf("%s\n", str);
     return 0;
 }
