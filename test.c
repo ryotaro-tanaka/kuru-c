b8763a491bb756f428afb8e8ff5a91d8888aef77
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-09 15:58:36 +0900

    17.2.4

--- test.c
+++ test.c
@@ -3,11 +3,13 @@
 int main(void)
 {
     FILE *file;
-    file = fopen("output.dat", "wb");
+    file = fopen("output.dat", "rb");
 
-    int buf = 100;
-    fwrite(&buf, sizeof(buf), 1, file);
+    int buf;
+    fread(&buf, sizeof(buf), 1, file);
 
     fclose(file);
+
+    printf("%d\n", buf);
     return 0;
 }
