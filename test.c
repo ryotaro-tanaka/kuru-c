a8b1e85fc02875cfb204c339df159c7cd609f68e
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-09 10:54:16 +0900

    17.1.3

--- test.c
+++ test.c
@@ -1,38 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef struct {
-    int year;
-    int clas;
-    int number;
-    char name[64];
-    double stature;
-    double weight;
-} student;
-
-void student_print(student *data);
 
 int main(void)
 {
-    student data;
-
-    // assignment
-    data.year = 3;
-    data.clas = 4;
-    data.number = 18;
-    strcpy(data.name, "MARIO");
-    data.stature = 168.2;
-    data.weight = 72.4;
-
-    // print
-    student_print(&data);
-
+    FILE *file;
+    file = fopen("output.txt", "w");
+    fprintf(file, "hello,world!!");
+    fclose(file);
     return 0;
-}
-
-void student_print(student *data)
-{
-    printf("%s\n", data->name);
-    printf("%f\n", data->stature);
-    return;
 }
