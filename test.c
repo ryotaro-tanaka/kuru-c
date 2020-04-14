3ec59e049a9b086e45e354ee75917122f5cd97e4
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-14 18:01:59 +0900

    18.t.4

--- test.c
+++ test.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
 
-#define PRINTM(X) printf("%d\n", X);
+int olympic(int year);
 
-int main(int argc, char *argv[])
+enum {
+    OLYMPIC_NON,
+    OLYMPIC_SUMMER,
+    OLYMPIC_WINTER,
+};
+
+int main(void)
 {
-    PRINTM(10);
+    int year;
+    scanf("%d", &year);
+
+    switch (olympic(year)) {
+    case OLYMPIC_NON:
+        printf("non\n");
+        break;
+    case OLYMPIC_SUMMER:
+        printf("summer\n");
+        break;
+    case OLYMPIC_WINTER:
+        printf("winter\n");
+        break;
+    default:
+        printf("?");
+        break;
+    };
     return 0;
+}
+
+int olympic(int year)
+{
+    if (year % 2 == 0) {
+        if (year % 4 == 0) {
+            return OLYMPIC_SUMMER;
+        } else {
+            return OLYMPIC_WINTER;
+        }
+        
+    } else {
+        return OLYMPIC_NON;
+    }
+
+    return -1;
 }
