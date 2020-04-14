27fd2fec92c2054cd6371fea3becfae28ffd1a62
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-14 22:16:48 +0900

    19.1.2

--- test.c
+++ test.c
@@ -1,47 +1,20 @@
 #include <stdio.h>
-
-int olympic(int year);
-
-enum {
-    OLYMPIC_NON,
-    OLYMPIC_SUMMER,
-    OLYMPIC_WINTER,
-};
+#include <stdlib.h>
 
 int main(void)
 {
-    int year;
-    scanf("%d", &year);
+    //int i;
+    int *heap;
+    heap = (int *)malloc(sizeof(int) * 10);
+    if (heap == NULL) exit(0);
 
-    switch (olympic(year)) {
-    case OLYMPIC_NON:
-        printf("non\n");
-        break;
-    case OLYMPIC_SUMMER:
-        printf("summer\n");
-        break;
-    case OLYMPIC_WINTER:
-        printf("winter\n");
-        break;
-    default:
-        printf("?");
-        break;
-    };
-    return 0;
-}
-
-int olympic(int year)
-{
-    if (year % 2 == 0) {
-        if (year % 4 == 0) {
-            return OLYMPIC_SUMMER;
-        } else {
-            return OLYMPIC_WINTER;
-        }
-        
-    } else {
-        return OLYMPIC_NON;
+    for (size_t i = 0; i < 10; i++)
+    {
+        heap[i] = i;
     }
+    printf("%d\n", heap[5]);
 
-    return -1;
+    free(heap);
+    
+    return 0;
 }
