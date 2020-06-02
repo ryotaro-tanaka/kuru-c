f14d4339b0b6703c4b8e85b1f9cbab312b773cc5
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-06-02 23:13:31 +0900

    21.1.3.1

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 // $ make
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
   char str[32];
 
   fgets(str, sizeof(str), stdin);
-  puts(str);
+
+  int val = atoi(str);
+  printf("%d\n", val*val);
   
   return 0;
 }
